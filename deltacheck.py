3e7f1154f390d5f2f81f72e75bbfc81aaeff6bac
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2022-07-26 14:42:15 -0400

    Update deltacheck.py

--- deltacheck.py
+++ deltacheck.py
@@ -1,5 +1,16 @@
+# Install pip3 install delta-spark==2.0.0
+# Install pip3 install pyspark==3.2.0
+
 from delta.tables import *
 from pyspark.sql.functions import *
+import pyspark
+from delta import *
+
+builder = pyspark.sql.SparkSession.builder.appName("PulsarApp") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
 df = spark.read.format("delta").load("/opt/demo/lakehouse/")
 df.printSchema()
